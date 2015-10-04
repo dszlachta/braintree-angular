b9e2957764f4b52755f33c6335fb3e2691d977fc
jeffcarp <gcarpenterv@gmail.com>
2015-10-04 19:05:28 +0000

    Refactor into more unit-testable modules

---
+++ test/unit/braintree-factory.js
@@ -0,0 +1,64 @@
+var assert = require('assert');
+var braintreeFactory = require('../../lib/braintree-factory');
+var jsdom = require('../../../jsdom');
+
+// TODO: See if using a mocking library would help
+var fakeBraintreeWeb = {
+  setup: function () {}
+};
+
+describe('braintreeFactory', function () {
+  describe('getClientToken', function () {
+
+    it('should call $http.get', function (done) {
+      var fake$httpGetCallCount = 0;
+      var fake$httpGetLastArg;
+
+      var fake$http = {
+        get: function (arg) {
+          fake$httpGetCallCount += 1;
+          fake$httpGetLastArg = arg;
+        }
+      };
+
+      var btNg = braintreeFactory(fakeBraintreeWeb)('/path', fake$http);
+
+      btNg.getClientToken();
+
+      console.log(btNg);
+
+      assert(fake$httpGetCallCount === 1);
+      assert(fake$httpGetLastArg === '/path');
+
+      done();
+    });
+
+    context('with customer_id', function () {
+
+      it('should call $http.get with customer_id', function (done) {
+        var fake$httpGetCallCount = 0;
+        var fake$httpGetLastArg;
+
+        var fake$http = {
+          get: function (arg) {
+            fake$httpGetCallCount += 1;
+            fake$httpGetLastArg = arg;
+          }
+        };
+
+        var btNg = braintreeFactory(fakeBraintreeWeb)('/path', fake$http);
+
+        btNg.getClientToken({customerId: '567'});
+
+        console.log(fake$httpGetLastArg);
+
+        assert(fake$httpGetCallCount === 1);
+        assert(fake$httpGetLastArg === '/path?customerId=567');
+
+        done();
+      });
+
+    });
+
+  });
+});
